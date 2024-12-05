5164fa17f9e8fd110a99daae9b719b788a1024df
saurabhsushir <saurabhsushir127@gmail.com>
2024-12-05 12:40:05 +0530

    Update follow-on.jsx

--- src/components/follow-on.jsx
+++ src/components/follow-on.jsx
@@ -7,13 +7,13 @@ const FollowOn = () => {
     >
       <span>Follow on:</span>
       <div className="flex gap-4 pt-3">
-        <a href="https://www.youtube.com/roadsidecoder">
+        <a href="https://www.youtube.com/@scalpingminds111">
           <FaYoutube size={20} />
         </a>
-        <a href="https://www.instagram.com/roadsidecoder">
+        <a href="https://www.instagram.com/scalpingminds111#">
           <FaInstagram size={20} />
         </a>
-        <a href="https://www.twitter.com/piyush_eon">
+        <a href="https://x.com/saurabh__sushir">
           <FaXTwitter size={20} />
         </a>
       </div>
